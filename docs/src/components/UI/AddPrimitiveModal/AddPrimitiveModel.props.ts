ba28b559e561a669a48acfdc3741329a09f8519e
Boris Okhota <95587232+borissska@users.noreply.github.com>
2025-06-03 15:29:34 +0500

    build added

--- docs/src/components/UI/AddPrimitiveModal/AddPrimitiveModel.props.ts
+++
@@ -1,7 +0,0 @@
-import { PrimitiveGroup } from "../../../types/primitive";
-
-export interface AddPrimitiveModalProps {
-  visible: boolean;
-  onCancel: () => void;
-  onSubmit: (group: PrimitiveGroup) => void;
-}
